9f74163e80b5cedca4b1c7575251da6e9cbb6c2b
berkahhariini999-rgb <berkahhariini999@gmail.com>
2026-06-21 15:28:07 +0700

    Initial Commit

---
+++ FoodUIApps/FoodUIAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodUIAppsApp.swift
+//  FoodUIApps
+//
+//  Created by Iqbal Alhadad on 21/06/26.
+//
+
+import SwiftUI
+
+@main
+struct FoodUIAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
